ada537dadc1707dd3877fef1e9bb0f487315ff7f
Jonathan Matsson <jmatss@kth.se>
2019-10-30 23:14:24 +0100

    Move create block/blocks logic from main.rs to create.rs

--- src/create.rs
+++ src/create.rs
@@ -1,9 +1,59 @@
-//use crate::block::Block;
+use std::error::Error;
+use std::time::Instant;
+use crate::block::Block;
+use crate::cons::HASH_SIZE;
+use crate::errors::GeneralError;
 
-//fn create_blocks(start: u64, end: u64, threads: u8, buffer_size: u64, filename: &str) {}
+pub fn create_blocks(
+    start: u64,
+    end: u64,
+    mut buffer_size: u64,
+    filename: String,
+) -> Result<Vec<Block>, Box<dyn Error>> {
+    let amount_of_hashes = end - start + 1;
+    let mut blocks: Vec<Block> = Vec::new();
 
-/*
-fn create_block() -> Box<Block> {
+    // Floor to multiple of HASH_SIZE.
+    buffer_size -= buffer_size % HASH_SIZE as u64;
+    if buffer_size == 0 {
+        return Err(Box::new(GeneralError::new(
+            format!("Specified buffer size is to small. Needs to be >= {} bytes.", HASH_SIZE)
+        )));
+    }
 
+    let mut current_start = start;
+    let mut current_end;
+
+    for i in 0.. {
+        current_end = current_start + (buffer_size / HASH_SIZE as u64);
+        if current_end > end {
+            current_end = end;
+        }
+
+        let mut start_time = Instant::now();
+        let mut b = Block::new(format!("{}{}", filename, i), current_start, current_end)?;
+        b.generate();
+        println!("Block{} generate: {} ms", i, start_time.elapsed().as_millis());
+
+        start_time = Instant::now();
+        b.sort();
+        println!("Block{} sort: {} ms", i, start_time.elapsed().as_millis());
+
+        start_time = Instant::now();
+        b.write_to_file()?;
+        println!("Block{} write_to_file: {} ms", i, start_time.elapsed().as_millis());
+
+        b.clear_hashes();
+
+        blocks.push(b);
+
+        if current_end == end {
+            break;
+        }
+
+        // Setup for next iteration of the for-loop.
+        current_start = current_end + 1;
+    }
+
+    return Ok(blocks);
 }
-*/
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,28 @@
-mod create;
-mod merge;
 mod block;
+mod cons;
+mod create;
 mod errors;
+mod merge;
 
 extern crate md5;
 
 use block::Block;
+use create::create_blocks;
 use std::time::Instant;
+use crate::cons::HASH_SIZE;
 
 fn main() {
     let start: u64 = 0;
     let end: u64 = 10_000_000;
+    let buffer_size: u64 = 10_000_000 * HASH_SIZE as u64;
 
+    let filename = String::from("list");
+
+    let start_time = Instant::now();
+    let blocks = create_blocks(start, end, buffer_size, filename).unwrap();
+    println!("DONE! Total elapsed: {} ms", start_time.elapsed().as_millis());
+
+    /*
     let mut start_time = Instant::now();
     let mut b = Block::new(String::from("test"), start, end).expect("Unable to create block.");
     b.generate();
@@ -24,4 +35,7 @@ fn main() {
         .expect("Unable to write to file");
     //println!("{:?}\n\n", b);
     println!("write_to_file: {}", start_time.elapsed().as_millis());
+
+    b.clear_hashes();
+    */
 }
