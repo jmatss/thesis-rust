343f534592e40fb3174d59660f8aa628227ef9ea
Jonathan Matsson <jmatss@kth.se>
2019-10-31 21:57:37 +0100

    Add merge logic to main

--- src/main.rs
+++ src/main.rs
@@ -6,21 +6,42 @@ mod merge;
 
 extern crate md5;
 
-use block::Block;
+use crate::cons::HASH_SIZE;
+use crate::merge::merge_blocks;
 use create::create_blocks;
 use std::time::Instant;
-use crate::cons::HASH_SIZE;
 
 fn main() {
     let start: u64 = 0;
-    let end: u64 = 10_000_000;
+    let end: u64 = 100;
+    //let end: u64 = 10_000_000;
     let buffer_size: u64 = 10_000_000 * HASH_SIZE as u64;
+    let amount_of_threads = 4;
+    let print_amount = 10_000_000;
+    let filename = "list";
 
-    let filename = String::from("list");
-
-    let start_time = Instant::now();
+    let total_time = Instant::now();
+    let mut sub_time = Instant::now();
     let blocks = create_blocks(start, end, buffer_size, filename).unwrap();
-    println!("DONE! Total elapsed: {} ms", start_time.elapsed().as_millis());
+    println!(
+        "Creating hashes done! Elapsed time: {} s",
+        sub_time.elapsed().as_secs()
+    );
+
+    sub_time = Instant::now();
+    merge_blocks(
+        blocks,
+        amount_of_threads,
+        buffer_size,
+        filename,
+        print_amount,
+    )
+    .expect("Unable to merge blocks.");
+    println!(
+        "Everything done! Merging elapsed time: {}, Total elapsed time: {} ms",
+        sub_time.elapsed().as_secs(),
+        total_time.elapsed().as_secs()
+    );
 
     /*
     let mut start_time = Instant::now();
