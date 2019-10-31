aaee020520ae47c5947fca048f79985f1aa94ed4
Jonathan Matsson <jmatss@kth.se>
2019-10-31 21:56:14 +0100

    Move BUF_SIZE to cons so that it can be used from multiple files

--- src/cons.rs
+++ src/cons.rs
@@ -1,3 +1,8 @@
+// Size in bytes(u8) of a md5 hash.
 pub const HASH_SIZE: usize = 16;
-// SSIDs of hashes starts at byte 10.
+
+// SSIDs in hashes starts at byte 10.
 pub const START_CMP: usize = 10;
+
+// Used by BufWriters and BufReaders.
+pub const BUF_SIZE: usize = 1 << 16;
