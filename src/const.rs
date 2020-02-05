c36fa7776e021727053282e55a6b8412471de0ad
jmatss <jmatss@kth.se>
2020-02-05 22:54:43 +0100

    Rename from cons to const

--- src/const.rs
+++ src/const.rs
@@ -8,7 +8,6 @@ pub const START_CMP: usize = 10;
 pub const BUF_SIZE: usize = 1 << 16;
 
 // Used by channels (arbitrary value).
-// Small buffer size so that not all merge_handler_threads gets depleted at the same time and
-// starts fetching hashes from the disk. That would mess with the "read proximity" of the disk
-// and would thus lower the possible IO throughput.
+// Small buffer size so that not all merge_handler_threads gets depleted at the
+// same time and starts fetching hashes from the disk.
 pub const CHAN_BUF_SIZE: usize = 16;
